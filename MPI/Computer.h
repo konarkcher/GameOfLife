943f03f25ba078894c3b2e83d5dde3350a52c9c7
Konarkcher <konarkcher@yandex.ru>
2018-12-31 03:38:28 +0300

    Add debug for dialogue

--- MPI/Computer.h
+++ MPI/Computer.h
@@ -54,19 +54,29 @@ private:
 
             std::vector<char> prev_field(ncol_), next_field(ncol_);
             if (rank_ % 2 == 0) {
+                std::cout << rank_ <<  " SENDS TO " << prev_ << '\n';
+                std::cout << rank_ <<  " SENDS TO " << next_ << '\n';
                 MPI_Send(field_->operator[](0), ncol_, MPI_CHAR, prev_, 0, MPI_COMM_WORLD);
                 MPI_Send(field_->operator[](nrow_ - 1), ncol_, MPI_CHAR, next_, 0, MPI_COMM_WORLD);
 
+                std::cout << rank_ <<  " RECEIVES FROM " << prev_ << '\n';
+                std::cout << rank_ <<  " RECEIVES FROM " << next_ << '\n';
                 MPI_Recv(&prev_field[0], ncol_, MPI_CHAR, prev_, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 MPI_Recv(&next_field[0], ncol_, MPI_CHAR, next_, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             } else {
                 if (rank_ == 1 && prev_ % 2 == 1) {
+                    std::cout << rank_ <<  " SENDS TO " << prev_ << '\n';
+                    std::cout << rank_ <<  " RECEIVES FROM " << prev_ << '\n';
                     MPI_Send(field_->operator[](0), ncol_, MPI_CHAR, prev_, 0, MPI_COMM_WORLD);
                     MPI_Recv(&prev_field[0], ncol_, MPI_CHAR, prev_, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 } else {
+                    std::cout << rank_ <<  " RECEIVES FROM " << prev_ << '\n';
+                    std::cout << rank_ <<  " SENDS TO " << prev_ << '\n';
                     MPI_Recv(&prev_field[0], ncol_, MPI_CHAR, prev_, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                     MPI_Send(field_->operator[](0), ncol_, MPI_CHAR, prev_, 0, MPI_COMM_WORLD);
                 }
+                std::cout << rank_ <<  " RECEIVES FROM " << next_ << '\n';
+                std::cout << rank_ <<  " SENDS TO " << next_ << '\n';
                 MPI_Recv(&next_field[0], ncol_, MPI_CHAR, next_, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 MPI_Send(field_->operator[](nrow_ - 1), ncol_, MPI_CHAR, next_, 0, MPI_COMM_WORLD);
             }
